806ca96a45b025d780b7915da21089ae6855b365
AkhilHector <akhilhector.1@gmail.com>
2015-08-31 17:22:13 +0530

    Added db module and main app to the code dir
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- db.js
+++ db.js
@@ -7,3 +7,25 @@ var m = require('mysql'),
     conf = require('./config'),
     usePooling = false;
 
+var pool = m.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: 'akhil',
+});
+
+module.exports.login = function() {
+        pool.getConnection(function(err, conn) {
+                conn.query("", function(err, rows) {
+                        conn.close();
+                });
+        });
+}
+
+module.exports.deleteAccount = function() {
+        pool.getConnection(function(err, conn) {
+                conn.query("", function(err, rows) {
+                        conn.close();
+                });
+        });
+}
+
