0ee970074b83804d653181ff80e7c91719a6bc00
AkhilHector <akhilhector.1@gmail.com>
2015-09-04 14:17:23 +0530

    Updated DB models, although functions to be added
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- db.js
+++ db.js
@@ -3,29 +3,99 @@
  * Team pclubGU
  * The MIT License
  */
-var m = require('mysql'),
-    conf = require('./config'),
-    usePooling = false;
+var m = require('rethinkdb'),
+    dbconf = require('./config').db,
+    useConnPooling = false,
+    connPool = null;
 
-var pool = m.createPool({
-        host: 'localhost',
-        user: 'root',
-        password: 'akhil',
-});
-
-module.exports.login = function() {
-        pool.getConnection(function(err, conn) {
-                conn.query("", function(err, rows) {
-                        conn.close();
+function connection(callback) {
+        if(useConnPooling) {
+                console.log("[LOGINFO] Starting a Pooled connection");
+                connPool.acquire(function(err, conn) {
+                        if(err) {
+                                callback(err);
+                        }
+                        else {
+                                console.log("[LOGINFO] Pooled a connection %s", conn._id);
+                        }
                 });
+        }
+
+        else {
+                r.connect({
+                        host: dbconf.host,
+                        port: dbconf.port
+                }, function(err, conn) {
+                        if(err) {
+                                console.log("[LOGERR] Couldnot connect to the port %s", dbconf['host'], dbconf['port']);
+                                callback(err);
+                        }
+                        else {
+                                conn.use(dbconf.db);
+                                console.log("[LOGINFO] Connected");
+                                callback(null, conn);
+                        }
+                });
+        }
+}
+
+function free(conn) {
+        console.log("[LOGINFO] Releasing the connection %s", conn._id);
+        if(useConnPooling) {
+                connPool.release(conn);
+        }
+        else {
+                conn.close();
+        }
+}
+
+function md5(str) {
+        return crypto.createHash('md5').update(str).digest('hex');
+}
+
+function gensalt() {
+        crypto.randomBytes('256', function(err, buf) {
+                if(err) throw err;
+                return buf;
         });
 }
 
-module.exports.deleteAccount = function() {
-        pool.getConnection(function(err, conn) {
-                conn.query("", function(err, rows) {
-                        conn.close();
-                });
-        });
+function saltandhash(pass, callback) {
+        var salt = gensalt();
+        callback(salt, md5(pass + salt));
 }
+
+if(typeof dbconf.pool == 'object') {
+        var g = require('generic-pool'),
+            pool = g.Pool({
+                    name: 'rethinkdb'),
+                    min: dbconf.pool.min || 5,
+                    max: dbconf.pool.max || 100,
+                    log: dbconf.pool.log || true,
+                    idleTimeoutMillis : dbconf.pool
+
+module.exports.login = function(callback) {
+        console.log("something");
+}
+
+module.exports.alogin = function(callback) {
+        console.log("something");
+}
+
+module.exports.getData = function(callback) {
+        console.log("something");
+}
+
+module.exports.accInfo = function(callback) {
+        console.log("something");
+}
+
+module.exports.updateAcc = function(callback) {
+        console.log("something");
+}
+
+module.exports.deleteAcc = function(callback) {
+        console.log("something");
+}
+
 
